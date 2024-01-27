34e3bade98f79f05bc55b35045aed09959c10d2f
Leo_Svjetlicic <lsvjetlicic@student.ferit.hr>
2024-01-27 13:23:54 +0100

    Add tests

---
+++ src/test/java/AuthTests.java
@@ -0,0 +1,71 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+class AuthTests {
+    // The tests are done with existing users because the app is connected to Firebase firestore
+    AndroidDriver driver;
+    WebElement usernameField;
+    WebElement passwordField;
+    WebElement loginButton;
+    WebElement changeScreenState;
+
+    @BeforeClass()
+    void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("app", "D:\\ChatApp\\ChatApp\\app\\build\\outputs\\apk\\debug\\app-debug.apk");
+        capabilities.setCapability("VERSION", "9.0");
+        capabilities.setCapability("deviceName", "TestPhone");
+        capabilities.setCapability("platformName", "Android");
+        driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        passwordField = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]"));
+        usernameField = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]"));
+        loginButton = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.Button"));
+        changeScreenState = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.TextView[3]"));
+
+        usernameField.sendKeys("leo.svjetlicic@gmail.com");
+        passwordField.sendKeys("");
+    }
+
+    @Test
+    void registerTestFail() {
+        changeScreenState.click();
+        loginButton.click();
+        driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+
+        Assert.assertTrue(passwordField.isDisplayed());
+    }
+    @Test
+    void loginTestFail() {
+        usernameField.sendKeys("leo.svjetlicic@l.com");
+        loginButton.click();
+        driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+
+//        Stays on the same screen
+        Assert.assertTrue(usernameField.isDisplayed());
+    }
+
+    @Test
+    void loginTestSuccess() {
+        driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
+        loginButton.click();
+        driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+
+//        Element on the next screen
+        WebElement nextScreenElement = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.TextView[3]"));
+        Assert.assertTrue(nextScreenElement.isDisplayed());
+    }
+    public void teardown() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/Tests.java
@@ -0,0 +1,106 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.graalvm.compiler.hotspot.replacements.Log;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+class Tests {
+    // The tests are done with existing users because the app is connected to Firebase firestore
+    AndroidDriver driver;
+    WebElement usernameField;
+    WebElement passwordField;
+    WebElement loginButton;
+
+    @BeforeClass()
+    void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("app", "D:\\ChatApp\\ChatApp\\app\\build\\outputs\\apk\\debug\\app-debug.apk");
+        capabilities.setCapability("VERSION", "9.0");
+        capabilities.setCapability("deviceName", "TestPhone");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("autoGrantPermissions", true);
+        driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        passwordField = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]"));
+        usernameField = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]"));
+        loginButton = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.Button"));
+    }
+
+    //    This is one test for the whole navigation in the app,
+    //    because I don't want to spam the firebase with login requests
+    @Test
+    void appTest() {
+        WebElement connectionElement;
+        WebElement addConnectionButton;
+        WebElement showQRCodeButton;
+        WebElement qrCodeText;
+        WebElement backButton;
+        WebElement camera;
+        WebElement inChatName;
+        WebElement chatTextInput;
+        WebElement sendMessageButton;
+        WebElement sentMessage;
+        WebElement heart;
+
+        usernameField.sendKeys("leo.svjetlicic@gmail.com");
+        passwordField.sendKeys("");
+        loginButton.click();
+        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+
+        showQRCodeButton = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[3]"));
+        connectionElement = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View"));
+
+        showQRCodeButton.click();
+        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+        qrCodeText = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.widget.TextView"));
+        backButton = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View[2]"));
+        Assert.assertTrue(qrCodeText.isDisplayed());
+        backButton.click();
+        driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
+        showQRCodeButton = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[3]"));
+        driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
+        Assert.assertTrue(showQRCodeButton.isDisplayed());
+
+        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+        addConnectionButton = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]"));
+        addConnectionButton.click();
+        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+        camera = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.View"));
+        Assert.assertTrue(camera.isDisplayed());
+        driver.navigate().back();
+        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
+        addConnectionButton = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]"));
+        Assert.assertTrue(addConnectionButton.isDisplayed());
+
+        connectionElement.click();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
+        inChatName = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View[2]/android.widget.TextView"));
+        sendMessageButton = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View[3]/android.view.View[2]"));
+        chatTextInput = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View[3]/android.widget.EditText"));
+        Assert.assertTrue(inChatName.isDisplayed());
+        chatTextInput.sendKeys("Kako da znam!!!");
+        sendMessageButton.click();
+        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+        sentMessage = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View[1]/android.view.View[1]/android.widget.TextView"));
+        Assert.assertEquals(sentMessage.getText(), "Kako da znam!!!");
+
+        sentMessage.click();
+        driver.manage().timeouts().implicitlyWait(200, TimeUnit.MILLISECONDS);
+        sentMessage.click();
+        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
+        heart = driver.findElement(By.xpath("(//android.view.View[@content-desc=\"Heart\"])[1]"));
+        Assert.assertTrue(heart.isDisplayed());
+    }
+
+    public void teardown() {
+        driver.quit();
+    }
+}
